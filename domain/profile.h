b3f23d9e8f633bd6b47d34e65a6fef81c4c5d9d3
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2023-01-25 16:16:20 +0900

    [add] profile構造体を定義

---
+++ domain/profile.h
@@ -0,0 +1,16 @@
+#ifndef EXP2_PROFILE_H
+#define EXP2_PROFILE_H
+
+#include "date.h"
+
+#define MAX_STR_LENGTH 70
+
+typedef struct {
+    int id;
+    char name[MAX_STR_LENGTH + 1];
+    Date *birthday;
+    char address[MAX_STR_LENGTH + 1];
+    char* comment;
+} Profile;
+
+#endif //EXP2_PROFILE_H
