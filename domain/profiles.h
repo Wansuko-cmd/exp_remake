ccaa31642108ce881652499705bc8b517ca4fd49
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2023-01-25 17:07:44 +0900

    [add] Profilesを追加

---
+++ domain/profiles.h
@@ -0,0 +1,14 @@
+#ifndef EXP2_PROFILES_H
+#define EXP2_PROFILES_H
+#include "profile.h"
+
+#define MAX_PROFILE_STORE_NUM 10000
+
+typedef struct {
+    Profile *data[MAX_PROFILE_STORE_NUM];
+    int size;
+} Profiles;
+
+void profiles_sort(Profiles target, int column);
+
+#endif //EXP2_PROFILES_H
